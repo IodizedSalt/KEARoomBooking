81e8d494ce12b49be46d8966ec048d5f536e955b
IodizedSalt <christophermoeller2000@gmail.com>
2018-09-27 10:34:39 +0200

    DB Schema setup with User and regNumber

--- KEARoomBooking/settings.py
+++ KEARoomBooking/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'LandingPage.apps.LandingpageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Copenhagen'
 
 USE_I18N = True
 
--- LandingPage/models.py
+++ LandingPage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+# Models are schemas/layout of the database
+
+class User(models.Model):
+    emailField = models.CharField(max_length=30)
+    nameField = models.CharField(max_length=20)
+    passwordField = models.CharField(max_length=20)
+
+
+class RegNum(models.Model):
+    userNum = models.ForeignKey(User, on_delete=models.CASCADE)
