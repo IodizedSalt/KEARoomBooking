779c5aa9783c8ad4c35d2c1ecc433331e149d1ed
IodizedSalt <christophermoeller2000@gmail.com>
2018-10-07 15:40:02 +0200

    registration form + functionality

--- LandingPage/views.py
+++ LandingPage/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -8,3 +9,15 @@ def index(request):
     # return HttpResponse("Hello World")
 
     return render(request, 'LandingPage/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/index')
+    else:
+        form = UserCreationForm()
+
+        args = {'form': form}
+        return render(request, 'LandingPage/index.html', args)
