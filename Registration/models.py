509a01da9ddfd4103da5e02eb0f3109c85001585
IodizedSalt <christophermoeller2000@gmail.com>
2018-11-22 21:04:18 +0100

    Migrations will not be pushed now

--- Registration/models.py
+++ Registration/models.py
@@ -1,84 +1,8 @@
 from django.contrib.auth.models import AbstractUser
-# # from django.db import models
-# #
-# #
-# # # Create your models here.
-# # # Models are schemas/layout of the database
-# #
-# # # class User(AbstractUser):
-# # #     email = models.CharField(max_length=30, unique=True)
-# # #     password1 = models.CharField(max_length=20)
-# # #     password2 = models.CharField(max_length=20)
-# # #     first_name = models.CharField(max_length=20)
-# # #     last_name = models.CharField(max_length=20)
-# # #
-# # #     def __str__(self):
-# # #         return self.email
+
 class User(AbstractUser):
-    # email = models.CharField(max_length=30, unique=True)
 
 
     def __str__(self):
-        return 'User: {} {} {} {} '.format(self.email, self.first_name, self.last_name, self.username)
-# from django.contrib.auth.base_user import BaseUserManager
-# from django.db import models
-# from django.contrib.auth.models import AbstractBaseUser
-# from django.contrib.auth.models import PermissionsMixin
-# from django.utils.translation import ugettext_lazy as _
-#
-#
-# class MyUserManager(BaseUserManager):
-#     """
-#     A custom user manager to deal with emails as unique identifiers for auth
-#     instead of usernames. The default that's used is "UserManager"
-#     """
-#     def _create_user(self, email, password, **extra_fields):
-#         """
-#         Creates and saves a User with the given email and password.
-#         """
-#         if not email:
-#             raise ValueError('The Email must be set')
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, **extra_fields)
-#         user.set_password(password)
-#         user.save()
-#         return user
-#
-#     def create_superuser(self, email, password, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#         extra_fields.setdefault('is_active', True)
-#
-#         if extra_fields.get('is_staff') is not True:
-#             raise ValueError('Superuser must have is_staff=True.')
-#         if extra_fields.get('is_superuser') is not True:
-#             raise ValueError('Superuser must have is_superuser=True.')
-#         return self._create_user(email, password, **extra_fields)
-#
-#
-# class User(AbstractBaseUser, PermissionsMixin):
-#     email = models.EmailField(unique=True, null=True)
-#     is_staff = models.BooleanField(
-#         _('staff status'),
-#         default=False,
-#         help_text=_('Designates whether the user can log into this site.'),
-#     )
-#     is_active = models.BooleanField(
-#         _('active'),
-#         default=True,
-#         help_text=_(
-#             'Designates whether this user should be treated as active. '
-#             'Unselect this instead of deleting accounts.'
-#         ),
-#     )
-#     USERNAME_FIELD = 'email'
-#     objects = MyUserManager()
-#
-#     def __str__(self):
-#         return self.email
-#
-#     def get_full_name(self):
-#         return self.email
-#
-#     def get_short_name(self):
-#         return self.email
+        # return 'User: {} {} {} {} '.format(self.email, self.first_name, self.last_name, self.username)
+        return self.email
